3e207842dd28975476d7bf2acc9db169bb8be6f1
joayeon <codinz.n@gmail.com>
2023-08-16 15:06:24 +0900

    feat: themeprovider 적용

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import { RouterProvider } from 'react-router-dom';
 import router from './Routes/routing';
 import { RecoilRoot } from 'recoil';
 import { ThemeProvider } from 'styled-components';
+import theme from './Styles/theme';
 
 function App() {
 	return (
 		<>
 			<RecoilRoot>
-				<ThemeProvider>
+				<ThemeProvider theme={theme}>
 					<RouterProvider router={router} />
 				</ThemeProvider>
 			</RecoilRoot>

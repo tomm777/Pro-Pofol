73e64fe6463cb1dc43fd20ebb40abc115b2a51a9
Hyeongyu-619 <110807563+Hyeongyu-619@users.noreply.github.com>
2023-08-16 14:55:15 +0900

    design: 공통 css 추가

--- src/Styles/global.js
+++ src/Styles/global.js
@@ -8,8 +8,30 @@ npm i styled-reset
 
 const GlobalStyles = createGlobalStyle`
     ${reset}
+
+    @font-face {
+        font-family: 'Pretendard-Light';
+        src: url('../../public/Assets/Font/Pretendard-Light.otf') format('opentype');
+    }
+
+    @font-face {
+        font-family: 'Pretendard-Regular';
+        src: url('../../public/Assets/Font/Pretendard-Regular.otf') format('opentype');
+    }
+
+    @font-face {
+        font-family: 'Pretendard-Medium';
+        src: url('../../public/Assets/Font/Pretendard-Medium.otf') format('opentype');
+    }
+
+    @font-face {
+        font-family: 'Pretendard-Bold';
+        src: url('../../public/Assets/Font/Pretendard-Bold.otf') format('opentype');
+    }
+
     * {
         box-sizing: border-box;
+        font-family: 'Pretendard-Regular';
     }
 
     button {

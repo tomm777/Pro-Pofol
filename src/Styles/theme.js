31eee8881b3266264414cf7ac0dbc9969615febc
joayeon <codinz.n@gmail.com>
2023-08-16 14:19:18 +0900

    design: 공통 css 추가

--- src/Styles/theme.js
+++ src/Styles/theme.js
@@ -1,1 +1,45 @@
-// test
+const PALETTE = {
+	mainColor: '#3377FF',
+	primary: {
+		100: '#E3EDFF',
+		200: '#99BBFF',
+	},
+
+	fontColor: '#FFFFFF',
+	hover: '#F6F6F6',
+
+	error: '#FFA620',
+
+	white: '#FFFFFF',
+	black: '#000000',
+	gray: {
+		100: '#EEEEEE',
+		200: '#E9E9E9',
+		300: '#585858',
+	},
+};
+
+const FONT_SIZE = {
+	es: '0.625rem',
+	xs: '0.75rem',
+	sm: '0.875rem',
+	md: '1rem',
+	big: '1.25rem',
+	lg: '1.5rem',
+	xl: '2.5rem',
+};
+
+const FONT_WEIGHT = {
+	light: '100',
+	regular: '400',
+	medium: '500',
+	bold: '700',
+};
+
+const theme = {
+	PALETTE,
+	FONT_SIZE,
+	FONT_WEIGHT,
+};
+
+export default theme;

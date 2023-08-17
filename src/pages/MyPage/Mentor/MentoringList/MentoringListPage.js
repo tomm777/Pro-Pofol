a308a11c33c82479598da8806776950a01af841d
[SW5] 박민준 <nami47351@gmail.com>
2023-08-18 04:09:21 +0900

    feat: 마이페이지 내 멘토링리스트페이지 추가

---
+++ src/pages/MyPage/Mentor/MentoringList/MentoringListPage.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import MentoringPage from '../../../../components/MyPage/RightContent/Mentor/MentoringPage';
+import * as M from './MentoringListPage.styles';
+import CardList from '../../../../components/MyPage/RightContent/Mentor/CardList/CardList';
+
+function MentoringListPage() {
+	const [totalCoaching, setTotalCoaching] = useState(0);
+	const [applyCoaching, setApplyCoaching] = useState(0);
+	const [completedCoaching, setCompletedCoaching] = useState(0);
+	const [refuseCoaching, setRefuseCoaching] = useState(0);
+
+	return (
+		<M.Wrapper>
+			<M.LightContent>
+				<MentoringPage
+					totalCoaching={totalCoaching}
+					applyCoaching={applyCoaching}
+					completedCoaching={completedCoaching}
+					refuseCoaching={refuseCoaching}
+				></MentoringPage>
+
+				<CardList total={totalCoaching}></CardList>
+			</M.LightContent>
+		</M.Wrapper>
+	);
+}
+
+export default MentoringListPage;

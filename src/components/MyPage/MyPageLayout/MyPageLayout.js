2f3d197e14e4c41be75fa61e8d19450fe27d714e
[SW5] 박민준 <nami47351@gmail.com>
2023-08-18 02:12:51 +0900

    feat. 마이페이지 레이아웃 & 디자인 추가

---
+++ src/components/MyPage/MyPageLayout/MyPageLayout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import SideMenu from '../SideMenu/SideMenu';
+import * as M from '../MyPageLayout/MyPageLayout.Styles';
+
+function MyPageLayout() {
+	return (
+		<>
+			<M.Wrapper>
+				<SideMenu userState={'OO멘토 님'}></SideMenu>
+				<Outlet />
+			</M.Wrapper>
+		</>
+	);
+}
+
+export default MyPageLayout;

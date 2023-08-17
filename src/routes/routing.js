face57d621090b28359497e960dc3ffd829be18b
[SW5] 박민준 <nami47351@gmail.com>
2023-08-18 04:02:54 +0900

    feat. 마이페이지 router 추가

--- src/routes/routing.js
+++ src/routes/routing.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import Layout from '../components/Layout';
 import Home from '../pages/Home/Home';
+import MyPage from '../pages/MyPage/MyPage';
+import WritingHistory from '../pages/MyPage/Mentor/WritingHistory/WritingHistory';
+import AccountManage from '../pages/MyPage/AccountManage/AccountManage';
+import AccountWithdrawal from '../pages/MyPage/AccountWithdrawal/AccountWithdrawal';
+import MyPageLayout from '../components/MyPage/MyPageLayout/MyPageLayout';
+import MentoringListPage from '../pages/MyPage/Mentor/MentoringList/MentoringListPage';
 
 const router = createBrowserRouter([
 	{
@@ -13,8 +19,30 @@ const router = createBrowserRouter([
 				element: <Home />,
 			},
 			{
-				path: '/MyPage',
-				element: <MyPage />,
+				path: 'MyPage',
+				element: <MyPageLayout />,
+				children: [
+					{
+						path: '',
+						element: <MyPage />,
+					},
+					{
+						path: 'mentoringlist',
+						element: <MentoringListPage />,
+					},
+					{
+						path: 'writinghistory',
+						element: <WritingHistory />,
+					},
+					{
+						path: 'AccountManage',
+						element: <AccountManage />,
+					},
+					{
+						path: 'AccountWithdrawal',
+						element: <AccountWithdrawal />,
+					},
+				],
 			},
 		],
 	},

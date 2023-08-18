b90b9f276b49993d55fa35fe64b189991c856ab7
[SW5] 예은선 <ysvln0912@gmail.com>
2023-08-18 16:09:10 +0900

    design: 스터디 페이지 퍼블리싱 진행 중

---
+++ src/pages/studyPage/studyPage.js
@@ -0,0 +1,21 @@
+import * as S from './studyPage.styles';
+import PopularCard from '../../components/studyPage/PopularCard/PopularCard';
+
+function StudyPage() {
+	return (
+		<>
+			<S.Container>
+				<S.TitleWrapper>
+					<S.Title>🔥 인기 스터디/ 프로젝트</S.Title>
+					<S.SubTitle>
+						지금 인기 있는 스터디, 프로젝트를 확인해보세요!
+					</S.SubTitle>
+				</S.TitleWrapper>
+
+				<PopularCard />
+			</S.Container>
+		</>
+	);
+}
+
+export default StudyPage;

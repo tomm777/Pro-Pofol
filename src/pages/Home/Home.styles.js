15604b3941ccdc1ca9cd363dbc7e0e7524296cc6
[SW5] 이혜진 <meggylee792@gmail.com>
2023-08-18 19:16:43 +0900

    feat:Mainhome 레이아웃 작업

--- src/pages/Home/Home.styles.js
+++ src/pages/Home/Home.styles.js
@@ -1,5 +1,47 @@
 import styled from 'styled-components';
+import { bodyContainer } from '../../styles/common';
 
 export const Wrap = styled.div`
-    
-`
+	width: 100%;
+`;
+
+export const Content = styled.div`
+	${bodyContainer}
+`;
+
+export const SlideBanner = styled.div`
+	width: 100%;
+	height: 300px;
+	background-color: lightgrey;
+	margin-top: 40px;
+	margin-bottom: 64px;
+`;
+
+export const RecommendedMentor = styled.div`
+	width: 100%;
+	margin-bottom: 64px;
+`;
+export const NewStudy = styled.div`
+	width: 100%;
+	margin-bottom: 64px;
+`;
+export const TitleBox = styled.div`
+	width: 100%;
+	display: flex;
+	justify-content: space-between;
+	margin-bottom: 32px;
+`;
+export const Title = styled.span``;
+export const ViewAll = styled.a`
+	color: ${({ theme }) => theme.PALETTE.mainColor};
+`;
+
+export const SlideStudyCard = styled.div`
+	width: 100%;
+	height: 200px;
+	background-color: lightgrey;
+`;
+
+export const PopularMento = styled.div`
+	width: 100%;
+`;

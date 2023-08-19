82937a9044bfb956c9374aa5270d194570be2d56
[SW5] 이혜진 <meggylee792@gmail.com>
2023-08-19 20:05:30 +0900

    design: 추천카드css수정

--- src/components/Home/RecommendCard/RecommendCard.js
+++ src/components/Home/RecommendCard/RecommendCard.js
@@ -1,45 +1,4 @@
-import styled from 'styled-components';
-
-const Container = styled.div`
-	display: flex;
-	width: 200px;
-	height: 260px;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	border-radius: 10px;
-	border: 1px solid #d2d2d2;
-	background: #fff;
-	margin-left: 20px;
-	margin-top: 32px;
-`;
-
-const NickName = styled.p`
-	color: #151618;
-	font-size: 18px;
-	font-weight: 700;
-	margin-bottom: 16px;
-`;
-const Company = styled.p`
-	color: #5e5f61;
-	font-size: 14px;
-	font-weight: 400;
-`;
-const Position = styled.p`
-	color: #7d7d7d;
-	font-size: 14px;
-	font-weight: 400;
-`;
-
-const Career = styled.p`
-	color: #7d7d7d;
-	font-size: 14px;
-	font-weight: 400;
-`;
-const ProfileImage = styled.img`
-	width: 80px;
-	height: 80px;
-`;
+import * from './Recommend.styles';
 
 RecommendCard.defaultProps = {
 	profileimage: './assets/img/profile/profile.png',

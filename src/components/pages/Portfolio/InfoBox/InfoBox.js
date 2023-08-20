de2360fa25f67ec5a6cc17b7280f7d75f1b78b4f
joayeon <codinz.n@gmail.com>
2023-08-21 01:01:47 +0900

    design: infobox 컴포넌트
    infobox 컴포넌트로 빼는 작업

---
+++ src/components/pages/Portfolio/InfoBox/InfoBox.js
@@ -0,0 +1,34 @@
+import Select from '../../../@common/Select/Select';
+import * as S from './InfoBox.styles';
+
+function InfoBox() {
+	const OPTIONS = [
+		{ value: 'select', name: '선택' },
+		{ value: 'frontend', name: '프론트엔드 개발' },
+		{ value: 'backend', name: '백엔드 개발' },
+		{ value: 'fullstack', name: '풀스택 개발' },
+		{ value: 'android', name: '안드로이드 개발' },
+		{ value: 'ios', name: 'IOS 개발' },
+	];
+
+	return (
+		<S.InfoBox>
+			<S.Contents>
+				<S.ContentsTitle>직무</S.ContentsTitle>
+				<Select options={OPTIONS} size={'regular'} font={'regular'} />
+			</S.Contents>
+
+			<S.Contents>
+				<S.ContentsTitle>이름</S.ContentsTitle>
+				<S.Input placeholder="이름을 입력해 주세요." />
+			</S.Contents>
+
+			<S.Contents>
+				<S.ContentsTitle>재직 회사</S.ContentsTitle>
+				<S.Input placeholder="회사명을 입력해 주세요." />
+			</S.Contents>
+		</S.InfoBox>
+	);
+}
+
+export default InfoBox;

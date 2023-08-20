6e63cd3002d53015cc8b4c1f33cf663f2e1c8ae2
joayeon <codinz.n@gmail.com>
2023-08-21 00:59:47 +0900

    design: Portfolio apply css 수정

---
+++ src/pages/Portfolio/PortfolioApply/PortfolioApply.js
@@ -0,0 +1,38 @@
+import { Textarea } from '../../../components/@common/Textarea/Textarea.styles';
+import InfoBox from '../../../components/pages/Portfolio/InfoBox/InfoBox';
+import * as S from './PortfolioApply.styles';
+
+function PortfolioApply() {
+	return (
+		<S.ApplyBox>
+			<S.TitleBox>
+				<span>멘토링 신청 게시물 작성</span>
+			</S.TitleBox>
+
+			<S.InfoContentsBox>
+				<S.ContentsBox>
+					<span>1. 멘토 님의 기본 정보를 작성해 주세요.</span>
+
+					<InfoBox />
+				</S.ContentsBox>
+
+				<S.ContentsBox>
+					<span>2. 멘토링 소개 제목을 작성해 주세요.</span>
+					<S.Inputs placeholder="제목을 입력해 주세요." />
+				</S.ContentsBox>
+
+				<S.ContentsBox>
+					<span>3. 멘토링 소개 내용을 작성해 주세요.</span>
+					<Textarea
+						size={'regular'}
+						placeholder={'소개 글 및 경력은 필수로 입력해 주세요.'}
+					/>
+				</S.ContentsBox>
+
+				<S.Button>등록하기</S.Button>
+			</S.InfoContentsBox>
+		</S.ApplyBox>
+	);
+}
+
+export default PortfolioApply;

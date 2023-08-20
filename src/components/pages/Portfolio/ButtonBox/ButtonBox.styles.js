8be445a269645788ee9ebdba8794b4b148d22797
joayeon <codinz.n@gmail.com>
2023-08-20 23:53:57 +0900

    design: buttonbox 컴포넌트
    buttonbox 컴포넌트로 빼는 작업

---
+++ src/components/pages/Portfolio/ButtonBox/ButtonBox.styles.js
@@ -0,0 +1,34 @@
+import { styled } from 'styled-components';
+
+export const ButtonBox = styled.div`
+	display: flex;
+	justify-content: space-between;
+	margin-bottom: 64px;
+
+	& div {
+		display: flex;
+		gap: 16px;
+	}
+`;
+
+export const Buttons = styled.button`
+	height: 50px;
+	padding: 10px 50px;
+	border-radius: 100px;
+
+	&:hover {
+		color: ${({ theme }) => theme.PALETTE.white};
+		background: ${({ theme }) => theme.PALETTE.mainColor};
+	}
+`;
+
+export const WriteButtons = styled(Buttons)`
+	border-radius: 10px;
+	color: ${({ theme }) => theme.PALETTE.white};
+	background: ${({ theme }) => theme.PALETTE.black};
+
+	&:hover {
+		color: ${({ theme }) => theme.PALETTE.black};
+		background: ${({ theme }) => theme.PALETTE.white};
+	}
+`;

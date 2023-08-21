c96538771de3dedf90347c09d5a5b033f6a2c85f
[SW5] 박민준 <nami47351@gmail.com>
2023-08-22 05:15:27 +0900

    feat: 멘토링 신청 게시물 작성 내역 서버 통신 , 리스트 삭제 기능 추가

--- src/components/pages/MyPage/RightContent/Mentor/MentoringHistoryPage/MentoringHistoryList.styles.js
+++ src/components/pages/MyPage/RightContent/Mentor/MentoringHistoryPage/MentoringHistoryList.styles.js
@@ -53,14 +53,6 @@ export const ContentListBox = styled.div`
 	}
 `;
 
-export const ContentList = styled.div`
-	display: flex;
-	width: 52.5rem;
-	align-items: flex-end;
-
-	border-bottom: 1px solid #d1d1d1;
-`;
-
 export const NothingContentList = styled.div`
 	display: flex;
 	width: 52.5rem;
@@ -70,73 +62,4 @@ export const NothingContentList = styled.div`
 	gap: 0.625rem;
 
 	border-top: 1px solid #000;
-`;
-
-export const ContentNumber = styled.div`
-	display: flex;
-	width: 3.75rem;
-	height: 5rem;
-	padding: 1.875rem 0rem;
-	justify-content: center;
-	align-items: center;
-	gap: 0.625rem;
-	flex-shrink: 0;
-`;
-
-export const ContentCategory = styled.div`
-	display: flex;
-	width: 7.5rem;
-	height: 5rem;
-	padding: 1.875rem 0rem;
-	justify-content: center;
-	align-items: center;
-	gap: 0.625rem;
-	flex-shrink: 0;
-`;
-
-export const ContentTitle = styled.div`
-	display: flex;
-	height: 5rem;
-	padding: 1.875rem 0rem;
-	justify-content: center;
-	align-items: center;
-	gap: 0.625rem;
-	flex: 1 0 0;
-	text-decoration-line: underline;
-	cursor: pointer;
-`;
-
-export const ContentDate = styled.div`
-	display: flex;
-	width: 10rem;
-	height: 5rem;
-	padding: 1.875rem 0rem;
-	justify-content: center;
-	align-items: center;
-	gap: 0.625rem;
-	flex-shrink: 0;
-`;
-
-export const ButtonBox = styled.div`
-	display: flex;
-	width: 6.25rem;
-	height: 5rem;
-	padding: 0.625rem 0rem;
-	justify-content: center;
-	align-items: center;
-	gap: 0.625rem;
-	flex-shrink: 0;
-`;
-
-export const DeleteButton = styled.button`
-	display: flex;
-	height: 1.875rem;
-	padding: 0.3125rem 0.9375rem 0.375rem 0.9375rem;
-	justify-content: center;
-	align-items: center;
-	gap: 0.625rem;
-	cursor: pointer;
-
-	border-radius: 0.25rem;
-	background: #e9e9e9;
 `;

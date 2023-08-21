a87d11a2a83bbde51f4e2ca14eb05f1fca271a09
[SW5] 이혜진 <meggylee792@gmail.com>
2023-08-21 18:02:32 +0900

    design: 회원가입 모달퍼블리싱

--- src/components/@common/Layout/Header/Header.js
+++ src/components/@common/Layout/Header/Header.js
@@ -1,26 +1,40 @@
+import React, { useState } from 'react';
 import * as S from './Header.styles';
+import SignupModal from '../../../pages/Home/Modal/SignUpModal';
 
 function Header() {
+	const [openModal, setOpenModal] = useState(false);
+
+	function handleSignupClick() {
+		setOpenModal(true);
+	}
+	function handleSignupClose() {
+		setOpenModal(false);
+	}
+
 	return (
-		<S.Header>
-			<S.ImgBox href="/">
-				<S.Image src="./assets/img/logo/logo.svg" />
-			</S.ImgBox>
+		<>
+			<S.Header>
+				<S.ImgBox href="/">
+					<S.Image src="./assets/img/logo/logo.svg" />
+				</S.ImgBox>
 
-			<S.NavBox>
-				<S.NavBar>
-					<a href="/">홈</a>
-					<a href="/portfolio">포트폴리오 리뷰</a>
-					<a href="/study">프로젝트 / 스터디 모집</a>
-				</S.NavBar>
+				<S.NavBox>
+					<S.NavBar>
+						<a href="/">홈</a>
+						<a href="/portfolio">포트폴리오 리뷰</a>
+						<a href="/study">프로젝트 / 스터디 모집</a>
+					</S.NavBar>
 
-				<S.LoginBar>
-					<a href="/login">로그인</a>
-					<a href="/signup">회원가입</a>
-					<S.Button>멘토 전환</S.Button>
-				</S.LoginBar>
-			</S.NavBox>
-		</S.Header>
+					<S.LoginBar>
+						<a href="/login">로그인</a>
+						<a onClick={handleSignupClick}>회원가입</a>
+						<S.Button>멘토 전환</S.Button>
+					</S.LoginBar>
+				</S.NavBox>
+			</S.Header>
+			{openModal && <SignupModal onClose={handleSignupClose} />}
+		</>
 	);
 }
 

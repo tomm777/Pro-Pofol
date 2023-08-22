ff432359431d36e6af01fb68afb7b29af4585f78
[SW5] 이혜진 <meggylee792@gmail.com>
2023-08-22 18:35:47 +0900

    feat: 백엔드로 코드 발급 요청으로 코드 변경

---
+++ src/pages/SignUp/SignUp.styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Wrap = styled.div`
+	width: 100%;
+	text-align: center;
+`;
+
+export const RegisterForm = styled.div`
+	margin-top: 120px;
+
+	> p {
+		font-size: 24px;
+		font-weight: 700;
+	}
+	> div > label {
+		font-size: 16px;
+		font-weight: 400;
+	}
+`;

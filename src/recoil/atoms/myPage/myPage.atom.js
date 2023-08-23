1f1ecd017faaab9aeece6b32cb763aa39a411dce
[SW5] 박민준 <nami47351@gmail.com>
2023-08-23 20:56:12 +0900

    feat: 마이페이지 멘토링 아이템 전역 변수 추가

---
+++ src/recoil/atoms/myPage/myPage.atom.js
@@ -0,0 +1,11 @@
+import { atom } from 'recoil';
+
+// export const mentoringState = atom({
+// 	key: 'mentoringState',
+// 	default: 'total',
+// });
+
+export const mentoringItem = atom({
+	key: 'mentoringItem',
+	default: [],
+});

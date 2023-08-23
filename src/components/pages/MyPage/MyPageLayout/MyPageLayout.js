0535f8d707699a791d80f81b438f98a458440b14
[SW5] 박민준 <nami47351@gmail.com>
2023-08-24 03:03:20 +0900

    feat: 마이페이지 로드시 유저 데이터 전달 받기

--- src/components/pages/MyPage/MyPageLayout/MyPageLayout.js
+++ src/components/pages/MyPage/MyPageLayout/MyPageLayout.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import SideMenu from '../SideMenu/SideMenu';
 import * as M from './MyPageLayout.Styles';
+import axios from 'axios';
 
 function MyPageLayout() {
+	const [user, setUser] = useState([]);
+	// 서버통신 (GET)
+	useEffect(() => {
+		async function getUserData() {
+			try {
+				const response = await axios.get(
+					'https://jsonplaceholder.typicode.com/todos/1',
+				);
+				setUser(response.data);
+			} catch (err) {
+				console.log(err);
+			}
+		}
+		getUserData();
+	}, []);
+
 	return (
 		<>
 			<M.Wrapper>
-				<SideMenu userState={'OO멘토 님'}></SideMenu>
+				<SideMenu
+					userState={
+						user.completed === true
+							? `${user.userId}님`
+							: `${user.userId}멘토님`
+					}
+				></SideMenu>
 				<Outlet />
 			</M.Wrapper>
 		</>

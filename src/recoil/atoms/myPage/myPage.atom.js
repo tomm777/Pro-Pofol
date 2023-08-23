eb46b2fe4e2a8aa2900807cb0a4b9707d69af460
[SW5] 박민준 <nami47351@gmail.com>
2023-08-24 02:59:04 +0900

    feat: 멘토링 아이템 전역 변수 선언

--- src/recoil/atoms/myPage/myPage.atom.js
+++ src/recoil/atoms/myPage/myPage.atom.js
@@ -1,11 +1,11 @@
 import { atom } from 'recoil';
 
-// export const mentoringState = atom({
-// 	key: 'mentoringState',
-// 	default: 'total',
-// });
-
 export const mentoringItem = atom({
 	key: 'mentoringItem',
-	default: [],
+	default: {
+		total: [],
+		apply: [],
+		completed: [],
+		refuse: [],
+	},
 });

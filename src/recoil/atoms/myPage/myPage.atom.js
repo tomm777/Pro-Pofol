138d5d0a0eaa80718cb31c153ae72e85cf0ee38d
[SW5] 박민준 <nami47351@gmail.com>
2023-08-24 11:14:51 +0900

    feat: 유저 데이터 전역 변수 선언

--- src/recoil/atoms/myPage/myPage.atom.js
+++ src/recoil/atoms/myPage/myPage.atom.js
@@ -1,4 +1,4 @@
-import { atom } from 'recoil';
+import { atom, atomFamily, selector, selectorFamily } from 'recoil';
 
 export const mentoringItem = atom({
 	key: 'mentoringItem',
@@ -9,3 +9,91 @@ export const mentoringItem = atom({
 		refuse: [],
 	},
 });
+
+// export const mentoringCategory = atom({
+// 	key: 'mentoringCategory',
+// 	default: {
+// 		total: 'total',
+// 		apply: 'apply',
+// 		completed: 'completed',
+// 		refuse: 'refuse',
+// 	},
+// });
+
+export const userData = atom({
+	key: 'userData',
+	default: {},
+});
+
+// export const modalState = atom({
+// 	key: 'modalState',
+// 	default: {
+// 		isOpen: false,
+// 	},
+// });
+
+// export const modalState = atomFamily({
+// 	key: 'modalState',
+// 	default: id => {
+// 		return {
+// 			id,
+// 			title: '',
+// 			isOpen: false,
+// 		};
+// 	},
+// 	// default: {
+// 	// 	myPage: {
+// 	// 		mentor: {
+// 	// 			edit: false,
+// 	// 			infoView: false,
+// 	// 			refuseEdit: false,
+// 	// 		},
+// 	// 		mentee: {
+// 	// 			editView: false,
+// 	// 			infoEdit: false,
+// 	// 			refuseView: false,
+// 	// 		},
+// 	// 	},
+// 	// },
+// });
+
+// const modalsAtomFamily = atomFamily({
+// 	key: 'modalsAtomFamily',
+// 	default: id => ({
+// 		id,
+// 		isOpen: false,
+// 		title: '',
+// 	}),
+// });
+
+// export const modalIdsAtom = atom({
+// 	key: 'modalIdsAtom',
+// 	default: [],
+// });
+
+// export const modalsSelectorFamily = selectorFamily({
+// 	key: 'modalsSelectorFamily',
+
+// 	get:
+// 		modalId =>
+// 		({ get }) =>
+// 			get(modalsAtomFamily(modalId)),
+
+// 	set:
+// 		modalId =>
+// 		({ get, set, reset }, modalInfo) => {
+// 			if (modalInfo) {
+// 				reset(modalsAtomFamily(modalId));
+// 				set(modalIdsAtom, prevValue =>
+// 					prevValue.filter(item => item !== modalId),
+// 				);
+
+// 				return;
+// 			}
+
+// 			set(modalsAtomFamily(modalId), modalInfo);
+// 			set(modalIdsAtom, prev =>
+// 				Array.from(new Set([...prev, modalInfo.id])),
+// 			);
+// 		},
+// });

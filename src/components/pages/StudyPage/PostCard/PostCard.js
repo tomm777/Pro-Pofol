9bb7da162f22a6aba80bb219c2245743741e3ff9
[SW5] 예은선 <ysvln0912@gmail.com>
2023-08-25 15:56:11 +0900

    feat: 스터디 게시글 컴포넌트 mock 데이터 연결

--- src/components/pages/StudyPage/PostCard/PostCard.js
+++ src/components/pages/StudyPage/PostCard/PostCard.js
@@ -1,36 +1,36 @@
 import * as S from './PostCard.styles';
+import { Link } from 'react-router-dom';
+import Chip from '../../../@common/Chip/Chip';
+import Line from '../../../@common/Line/Line';
 
-function PostCard() {
+function PostCard({ data }) {
+	// console.log(data);
+
+	const { postId, status, title, content, position, category } = data;
+
 	return (
 		<S.Container>
-			<S.ChipWrapper>
-				<S.ChipBox>
-					<S.ChipText>프로젝트</S.ChipText>
-				</S.ChipBox>
-				<S.ChipBox>
-					<S.ChipText>모집중</S.ChipText>
-				</S.ChipBox>
-			</S.ChipWrapper>
+			<Link to={`/study/detail/${postId}`}>
+				<S.ChipWrapper>
+					<Chip category={category} />
+					<Chip status={status} />
+				</S.ChipWrapper>
 
-			<S.Title>스터디 및 프로젝트 제목이 들어가는 공간입니다.</S.Title>
-			<S.PostText>
-				스터디 내용 및 프로젝트 내용이 들어가는 공간입니다. 스터디 내용
-				및 프로젝트 내용이 들어가는 공간입니다. 스터디 내용 및 프로젝트
-				내용이 들어가는 공간입니다. 스터디 내용 및 프로젝트 내용이
-				들어가는 공간입니다. 스터디 내용 및 프로젝트 내용이 들어가는
-				공간입니다. 스터디 내용 및 프로젝트 내용이 들어가는 공간입니다.
-				스터디 내용 및 프로젝트 내용이 ...
-			</S.PostText>
+				<S.Title>{title}</S.Title>
+				<S.PostText>{content}</S.PostText>
 
-			<S.BottomBox>
-				<S.RightBox>
-					<S.Position>프론트엔드</S.Position>
-					<S.Position>프론트엔드</S.Position>
-					<S.Position>프론트엔드</S.Position>
-				</S.RightBox>
+				<S.BottomBox>
+					<S.RightBox>
+						{position.map((name, idx) => (
+							<S.PositionBox key={idx}>
+								<S.Position>{name}</S.Position>
+							</S.PositionBox>
+						))}
+					</S.RightBox>
 
-				<S.Day>마감 10일 전</S.Day>
-			</S.BottomBox>
+					<S.Day>마감 10일 전</S.Day>
+				</S.BottomBox>
+			</Link>
 		</S.Container>
 	);
 }

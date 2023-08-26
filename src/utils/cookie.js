dbaa009a4bd670aab67b8cf6bb21d815d6b51d7c
[SW5] 이혜진 <meggylee792@gmail.com>
2023-08-26 17:48:04 +0900

    comment: 주석 추가

--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -1,3 +1,5 @@
+// 쿠키에서  key 값을 넣으면 key값에 맞는 value를 찾아주는 함수
+
 export const getCookie = targetKey => {
 	const cookies = document.cookie.split('; ').map(cookie => {
 		const [key, value] = cookie.split('=');

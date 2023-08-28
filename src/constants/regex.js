fb898ffd3f1aa76900c2f013319a588d48358c8d
joayeon <codinz.n@gmail.com>
2023-08-28 23:01:35 +0900

    feat: nickName 정규식 추가

--- src/constants/regex.js
+++ src/constants/regex.js
@@ -4,6 +4,7 @@ const VALIDATE = {
 	password:
 		/^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/,
 	phone: /^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/,
+	nickName: /^[ㄱ-ㅎ가-힣a-zA-Z0-9]+$/,
 };
 
 export default VALIDATE;

ea391852eee461d3c557cb4178e7ad00d708425d
[SW5] 박민준 <nami47351@gmail.com>
2023-08-29 16:18:02 +0900

    feat: 마이페이지 유저 정보 받아오기 & 역할 부여

--- src/components/pages/MyPage/MyPageLayout/MyPageLayout.js
+++ src/components/pages/MyPage/MyPageLayout/MyPageLayout.js
@@ -2,62 +2,35 @@ import React, { useState, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import SideMenu from '../SideMenu/SideMenu';
 import * as M from './MyPageLayout.Styles';
-import axios from 'axios';
-import { useRecoilState, useRecoilValue } from 'recoil';
-import { userData } from '../../../../recoil/atoms/myPage/myPage.atom';
 import useFooter from '../../../../hooks/useFooter';
+import useApi from '../../../../hooks/useApi';
 
 function MyPageLayout() {
-	useFooter(); // 마이페이지 푸터 제거
-	const [user, setUser] = useRecoilState(userData);
+	// 마이페이지 내 모든 푸터 삭제
+	useFooter();
 
-	function getCookie(name) {
-		const nameOfCookie = name + '=';
-		let x = 0;
+	// 유저 정보 담을 state
+	const [user, setUser] = useState({});
+	// 유저 정보 통신(GET)
+	const { result, trigger, isLoading, error } = useApi({
+		path: `/user`,
+		shouldFetch: true,
+	});
 
-		while (x <= document.cookie.length) {
-			const y = x + nameOfCookie.length;
-			let endOfCookie = document.cookie.indexOf(';', y);
-			if (document.cookie.substring(x, y) === nameOfCookie) {
-				if (endOfCookie === -1) endOfCookie = document.cookie.length;
-
-				return unescape(document.cookie.substring(y, endOfCookie));
-			}
-
-			x = document.cookie.indexOf(' ', x) + 1;
-
-			if (x === 0) break;
-		}
-
-		return '';
-	}
-
-	const emailCookie = getCookie('email');
-	console.log(emailCookie);
-
-	// 서버통신(GET);
+	// 유저 정보 state에 저장
 	useEffect(() => {
-		async function getUserData() {
-			try {
-				const response = await axios.get(
-					`http://localhost:8080/api/user/${emailCookie}`,
-				);
-				setUser(response.data);
-				console.log(response.data);
-			} catch (err) {
-				console.log(err);
-			}
+		if (result) {
+			setUser(result);
 		}
-		getUserData();
-	}, []);
+	}, [result]);
 
 	return (
 		<M.Wrapper>
 			<SideMenu
 				userState={
-					!user.completed // 임시
-						? `${user.userId} 멘토님`
-						: `${user.userId} 님`
+					user.role === 'mentor'
+						? `${user.name} 멘토님`
+						: `${user.name} 님`
 				}
 			></SideMenu>
 			<Outlet />

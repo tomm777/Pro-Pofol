6594cc35fccb2dbffd1ccb1664d52044cf814518
[SW5] 이혜진 <meggylee792@gmail.com>
2023-08-30 14:10:04 +0900

    feat: api호출 try~ catch 구문 삭제

--- src/components/@common/Layout/Header/Header.js
+++ src/components/@common/Layout/Header/Header.js
@@ -30,16 +30,12 @@ function Header() {
 	const { trigger } = useApi({});
 
 	const handleLogoutClick = async () => {
-		try {
-			trigger({ path: '/auth/logout', method: 'post' });
+		trigger({ path: '/auth/logout', method: 'post' });
 
-			setIsLoggedIn(false);
-			window.alert('로그아웃 되었습니다 ');
-			window.location.reload();
-			navigate('/');
-		} catch (error) {
-			console.error('로그아웃 오류:', error);
-		}
+		setIsLoggedIn(false);
+		window.alert('로그아웃 되었습니다 ');
+		window.location.reload();
+		navigate('/');
 	};
 
 	return (

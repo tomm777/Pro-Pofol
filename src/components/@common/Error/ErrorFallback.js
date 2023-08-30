bf57f2e6a8916d60795a9502b4be57f4a22f20ff
joayeon <codinz.n@gmail.com>
2023-08-31 04:51:18 +0900

    design: error page 디자인 작업

--- src/components/@common/Error/ErrorFallback.js
+++ src/components/@common/Error/ErrorFallback.js
@@ -1,14 +1,33 @@
+import useFooter from '../../../hooks/useFooter';
+import Button from '../Button/Button';
 import * as S from './ErrorFallback.styles';
 
 function ErrorFallback({ error }) {
+	useFooter();
 	console.log(error);
 
 	return (
 		<S.ErrorBox>
-			<div>에러가 발생했습니다!</div>
-			<div>요청하신 페이지를 찾을 수 없습니다.</div>
+			<div>
+				<img src="/assets/img/icons/error.png" />
+			</div>
 
-			<a>홈으로 이동</a>
+			<S.LetterBox>
+				<div>에러가 발생했습니다!</div>
+				<div>요청하신 페이지를 찾을 수 없습니다.</div>
+			</S.LetterBox>
+
+			<S.ButtonBox>
+				<a href="/">
+					<Button
+						variant={'reverse'}
+						shape={'medium'}
+						size={'medium'}
+					>
+						홈으로 이동
+					</Button>
+				</a>
+			</S.ButtonBox>
 		</S.ErrorBox>
 	);
 }

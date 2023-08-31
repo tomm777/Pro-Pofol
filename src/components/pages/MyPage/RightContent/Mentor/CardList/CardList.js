ac994ed2d883e09b756e061a451ba41c2d67861b
[SW5] 박민준 <nami47351@gmail.com>
2023-09-01 05:33:02 +0900

    fix: 마이페이지 내 통신및 기능 추가

--- src/components/pages/MyPage/RightContent/Mentor/CardList/CardList.js
+++ src/components/pages/MyPage/RightContent/Mentor/CardList/CardList.js
@@ -1,10 +1,10 @@
 import * as CL from './CardList.styles';
 import ApplicationCard from '../Card/Total/ApplicationCard';
 import { useEffect, useState } from 'react';
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilValue } from 'recoil';
 import {
 	mentoringItem,
-	userData,
+	applyItem,
 } from '../../../../../../recoil/atoms/myPage/myPage.atom';
 import MYPAGEOPTION from '../../../../../../constants/mypage';
 import MESSAGE from '../../../../../../constants/message';
@@ -12,78 +12,152 @@ import useApi from '../../../../../../hooks/useApi';
 
 // 카드 리스트
 function CardList() {
+	// 멘토링 신청 받은 내역, 멘토링 신청 내역
 	const mentoringData = useRecoilValue(mentoringItem);
+	const applyData = useRecoilValue(applyItem);
 
-	const requestedLength = mentoringData?.requested?.length;
-	const acceptedLength = mentoringData?.accepted?.length;
-	const completedLength = mentoringData?.completed?.length;
-	const rejectedLength = mentoringData?.rejected?.length;
-	const subTitles = {
+	// 멘토링 신청 받은 내역, 멘토링 신청 내역 길이 리스트
+	const itemLength = {
 		mentor: {
-			requested: MYPAGEOPTION.MENTOR.SUBTITLE.REQUESTED,
-			accepted: MYPAGEOPTION.MENTOR.SUBTITLE.ACCEPTED,
-			completed: MYPAGEOPTION.MENTOR.SUBTITLE.COMPLETED,
-			rejected: MYPAGEOPTION.MENTOR.SUBTITLE.REJECTED,
+			requested: mentoringData?.requested?.length || 0,
+			accepted: mentoringData?.accepted?.length || 0,
+			completed: mentoringData?.completed?.length || 0,
+			rejected: mentoringData?.rejected?.length || 0,
 		},
 		mentee: {
-			requested: MYPAGEOPTION.MENTEE.SUBTITLE.REQUESTED,
-			accepted: MYPAGEOPTION.MENTEE.SUBTITLE.ACCEPTED,
-			completed: MYPAGEOPTION.MENTEE.SUBTITLE.COMPLETED,
-			rejected: MYPAGEOPTION.MENTEE.SUBTITLE.REJECTED,
+			requested: applyData?.requested?.length || 0,
+			accepted: applyData?.accepted?.length || 0,
+			completed: applyData?.completed?.length || 0,
+			rejected: applyData?.rejected?.length || 0,
 		},
 	};
 
+	// 카테고리 부제목 리스트
+	const subTitles = {
+		mentor: {
+			requested: MYPAGEOPTION?.MENTOR?.SUBTITLE?.REQUESTED || '',
+			accepted: MYPAGEOPTION?.MENTOR?.SUBTITLE?.ACCEPTED || '',
+			completed: MYPAGEOPTION?.MENTOR?.SUBTITLE?.COMPLETED || '',
+			rejected: MYPAGEOPTION?.MENTOR?.SUBTITLE?.REJECTED || '',
+		},
+		mentee: {
+			requested: MYPAGEOPTION?.MENTEE?.SUBTITLE?.REQUESTED || '',
+			accepted: MYPAGEOPTION?.MENTEE?.SUBTITLE?.ACCEPTED || '',
+			completed: MYPAGEOPTION?.MENTEE?.SUBTITLE?.COMPLETED || '',
+			rejected: MYPAGEOPTION?.MENTEE?.SUBTITLE?.REJECTED || '',
+		},
+	};
+
+	// 현재 누른 카테고리 설정
 	const [category, setCategory] = useState('');
 	const buttonHandler = e => {
 		setCategory(e.target.id);
 	};
 
+	// 유저 정보 담을 state
+	const [user, setUser] = useState({});
+	// 유저 정보 통신(GET)
+	const { result: users } = useApi({
+		path: `/user`,
+		shouldFetch: true,
+	});
+
+	// 유저 정보가 변경될 때 리렌더링
+	useEffect(() => {
+		if (users) {
+			setUser(users);
+		}
+	}, [users]);
+
 	return (
 		<>
-			{category === 'accepted' ? (
-				<CardListLayout
-					length={acceptedLength}
-					fun={buttonHandler}
-					categoryKey={category}
-					subTitles={subTitles}
-				/>
-			) : category === 'completed' ? (
-				<CardListLayout
-					length={completedLength}
-					fun={buttonHandler}
-					categoryKey={category}
-					subTitles={subTitles}
-				/>
-			) : category === 'rejected' ? (
-				<CardListLayout
-					length={rejectedLength}
-					fun={buttonHandler}
-					categoryKey={category}
-					subTitles={subTitles}
-				/>
-			) : (
-				<CardListLayout
-					length={requestedLength}
-					fun={buttonHandler}
-					categoryKey={category}
-					subTitles={subTitles}
-				/>
-			)}
+			{users.role === 'mentor' ? (
+				<>
+					{category === 'accepted' ? (
+						<CardListLayout
+							length={itemLength.mentor.accepted}
+							fun={buttonHandler}
+							categoryKey={category}
+							subTitles={subTitles}
+							item={mentoringData.accepted}
+						/>
+					) : category === 'completed' ? (
+						<CardListLayout
+							length={itemLength.mentor.completed}
+							fun={buttonHandler}
+							categoryKey={category}
+							subTitles={subTitles}
+							item={mentoringData.completed}
+						/>
+					) : category === 'rejected' ? (
+						<CardListLayout
+							length={itemLength.mentor.rejected}
+							fun={buttonHandler}
+							categoryKey={category}
+							subTitles={subTitles}
+							item={mentoringData.rejected}
+						/>
+					) : (
+						<CardListLayout
+							length={itemLength.mentor.requested}
+							fun={buttonHandler}
+							categoryKey={category}
+							subTitles={subTitles}
+							item={mentoringData.requested}
+						/>
+					)}
+				</>
+			) : users.role === 'user' ? (
+				<>
+					{category === 'accepted' ? (
+						<CardListLayout
+							length={itemLength.mentee.accepted}
+							fun={buttonHandler}
+							categoryKey={category}
+							subTitles={subTitles}
+							item={applyData.accepted}
+						/>
+					) : category === 'completed' ? (
+						<CardListLayout
+							length={itemLength.mentee.completed}
+							fun={buttonHandler}
+							categoryKey={category}
+							subTitles={subTitles}
+							item={applyData.completed}
+						/>
+					) : category === 'rejected' ? (
+						<CardListLayout
+							length={itemLength.mentee.rejected}
+							fun={buttonHandler}
+							categoryKey={category}
+							subTitles={subTitles}
+							item={applyData.rejected}
+						/>
+					) : (
+						<CardListLayout
+							length={itemLength.mentee.requested}
+							fun={buttonHandler}
+							categoryKey={category}
+							subTitles={subTitles}
+							item={applyData.requested}
+						/>
+					)}
+				</>
+			) : undefined}
 		</>
 	);
 }
 
 export default CardList;
 
-function CardListLayout({ length, fun, categoryKey, subTitles }) {
+function CardListLayout({ length, fun, categoryKey, subTitles, item }) {
 	// 유저 정보 담을 state
 	const [user, setUser] = useState({});
 	// 유저 정보 통신(GET)
-	const { result: users, trigger: usersT } = useApi({
+	const { result: users } = useApi({
 		path: `/user`,
 		shouldFetch: true,
 	});
-
 	// 유저 정보가 변경될 때 리렌더링
 	useEffect(() => {
 		if (users) {
@@ -192,23 +266,26 @@ function CardListLayout({ length, fun, categoryKey, subTitles }) {
 					</CL.SubTitleFlexBox>
 					<p>총 {length}건</p>
 				</CL.SubContentBar>
-
 				{length === 0 ? (
-					<CL.NonSubContentListBox>
-						{MESSAGE.MYPAGE.NODATA.LIST}
-					</CL.NonSubContentListBox>
+					<>
+						<CL.NonSubContentListBox>
+							{MESSAGE.MYPAGE.NODATA.LIST}
+						</CL.NonSubContentListBox>
+					</>
 				) : (
-					<CL.SubContentListBox>
-						{users.role === 'mentor' ? (
-							<ApplicationCard
-								categoryKey={categoryKey}
-							></ApplicationCard>
-						) : (
-							<ApplicationCard
-								categoryKey={categoryKey}
-							></ApplicationCard>
-						)}
-					</CL.SubContentListBox>
+					<>
+						<CL.SubContentListBox>
+							{item.map((element, index) => {
+								return (
+									<ApplicationCard
+										key={index}
+										categoryKey={categoryKey}
+										item={element}
+									></ApplicationCard>
+								);
+							})}
+						</CL.SubContentListBox>
+					</>
 				)}
 			</CL.SubContentBox>
 		</>
--- src/pages/MyPage/Mentor/MentoringList/MentoringListPage.js
+++ src/pages/MyPage/Mentor/MentoringList/MentoringListPage.js
@@ -10,27 +10,13 @@ import { useRecoilState } from 'recoil';
 import useApi from '../../../../hooks/useApi';
 
 function MentoringListPage() {
+	const [mentoringData, setMentoringData] = useRecoilState(mentoringItem);
+	const [applyData, setApplyData] = useRecoilState(applyItem);
 	// 유저 정보 담을 state
 	const [user, setUser] = useState({});
 	// 유저 정보 통신(GET)
-	const { result: users, trigger: usersT } = useApi({
+	const { result: users } = useApi({
 		path: `/user`,
-		shouldFetch: true,
-	});
-
-	// 멘토링 신청 정보 담을 state
-	const [mentoringData, setMentoringData] = useRecoilState(mentoringItem);
-	// 멘토링 신청 정보 통신(GET)
-	const { result: mentoringDatas, trigger: mentoringDatasT } = useApi({
-		path: `/portfolio/mentor/mentoringRequests`,
-		shouldFetch: true,
-	});
-
-	// 멘토링 신청한 정보 담을 state
-	const [applyData, setApplyData] = useRecoilState(applyItem);
-	// 멘토링 신청한 정보 통신(GET)
-	const { result: applyDatas, trigger: applyDatasT } = useApi({
-		path: `/portfolio/user/myMentoringRequests`,
 		shouldFetch: true,
 	});
 
@@ -39,64 +25,193 @@ function MentoringListPage() {
 		if (users) {
 			setUser(users);
 		}
-		console.log('user정보: ', user);
 	}, [users]);
 
-	// 멘토링 신청 정보가 변경될 때 리렌더링
-	useEffect(() => {
-		if (mentoringDatas && mentoringDatas.length > 0) {
-			// const totalData = mentoringDatas.filter(
-			// 	item => item.status === 'requested',
-			// );
-			const requestedData = mentoringDatasT({
-				path: `/portfolio/mentor/mentoringRequests/?status=requested`,
-			});
-			const acceptedData = mentoringDatasT({
-				path: `/portfolio/mentor/mentoringRequests/?status=apply`,
-			});
-			const completedData = mentoringDatasT({
-				path: `/portfolio/mentor/mentoringRequests/?status=completed`,
-			});
-			const rejectedData = mentoringDatasT({
-				path: `/portfolio/mentor/mentoringRequests/?status=refuse`,
-			});
-			setMentoringData({
-				requested: requestedData,
-				accepted: acceptedData,
-				completed: completedData,
-				rejected: rejectedData,
-			});
-		}
-		console.log('멘토 신청 받은 내역 : ', mentoringDatas);
-		console.log('멘토 신청 받은 내역 : ', mentoringData);
-	}, [mentoringDatas]);
+	const statuses = ['requested', 'accepted', 'completed', 'rejected'];
+	// Dynamic api requests for each status and type (mentor/user)
+	statuses.forEach(status => {
+		// Mentor data request
+		const { result: mentoringResult } = useApi({
+			path: `/portfolio/mentor/mentoringRequests?status=${status}`,
+			shouldFetch: true,
+		});
 
-	// 멘토링 신청 정보가 변경될 때 리렌더링
-	useEffect(() => {
-		if (applyDatas && applyDatas.length > 0) {
-			setApplyData(applyDatas);
-			const requestedData = applyDatasT({
-				path: `/portfolio/user/myMentoringRequests/?status=requested`,
-			});
-			const acceptedData = applyDatasT({
-				path: `/portfolio/user/myMentoringRequests/?status=apply`,
-			});
-			const completedData = applyDatasT({
-				path: `/portfolio/user/myMentoringRequests/?status=completed`,
-			});
-			const rejectedData = applyDatasT({
-				path: `/portfolio/user/myMentoringRequests/?status=refuse`,
-			});
-			setApplyData({
-				requested: requestedData,
-				accepted: acceptedData,
-				completed: completedData,
-				rejected: rejectedData,
-			});
-		}
-		console.log('유저 신청 내역 : ', applyDatas);
-		console.log('유저 신청 내역 : ', applyData);
-	}, [applyDatas]);
+		useEffect(() => {
+			if (mentoringResult && mentoringResult.length > 0) {
+				setMentoringData(prevState => ({
+					...prevState,
+					[status]: mentoringResult,
+				}));
+			}
+		}, [mentoringResult]);
+
+		// User data request
+		const { result: applyResult } = useApi({
+			path: `/portfolio/user/myMentorinRequests?status=${status}`,
+			shouldFetch: true,
+		});
+
+		useEffect(() => {
+			if (applyResult && applyResult.length > 0) {
+				setApplyData(prevState => ({
+					...prevState,
+					[status]: applyResult,
+				}));
+			}
+		}, [applyResult]);
+	});
+
+	// // 멘토 신청 받은 건
+	// const [requestedMentoringData, setRequestedMentoringData] = useState([]);
+	// const { result: requestedMentoringDatas } = useApi({
+	// 	path: `/portfolio/mentor/mentoringRequests?status=requested`,
+	// 	shouldFetch: true,
+	// });
+
+	// useEffect(() => {
+	// 	if (requestedMentoringDatas && requestedMentoringDatas.length > 0) {
+	// 		setRequestedMentoringData(requestedMentoringDatas);
+	// 	}
+	// 	// console.log('(멘토)신청 받은 건: ', requestedMentoringData);
+	// }, [requestedMentoringDatas]);
+
+	// // 멘토 신청 수락 건
+	// const [acceptedMentoringData, setAcceptedMentoringData] = useState([]);
+	// const { result: acceptedMentoringDatas } = useApi({
+	// 	path: `/portfolio/mentor/mentoringRequests?status=accepted`,
+	// 	shouldFetch: true,
+	// });
+
+	// useEffect(() => {
+	// 	if (acceptedMentoringDatas && acceptedMentoringDatas.length > 0) {
+	// 		setAcceptedMentoringData(acceptedMentoringDatas);
+	// 	}
+	// 	// console.log('(멘토)신청 수락 건: ', acceptedMentoringData);
+	// }, [acceptedMentoringDatas]);
+
+	// // 멘토 리뷰 완료 건
+	// const [completedMentoringData, setCompletedMentoringData] = useState([]);
+	// const { result: completedMentoringDatas } = useApi({
+	// 	path: `/portfolio/mentor/mentoringRequests?status=completed`,
+	// 	shouldFetch: true,
+	// });
+
+	// useEffect(() => {
+	// 	if (completedMentoringDatas && completedMentoringDatas.length > 0) {
+	// 		setCompletedMentoringData(completedMentoringDatas);
+	// 	}
+	// 	// console.log('(멘토)리뷰 완료 건: ', completedMentoringData);
+	// }, [completedMentoringDatas]);
+
+	// // 멘토 신청 거절 건
+	// const [rejectedMentoringData, setRejectedMentoringData] = useState([]);
+	// const { result: rejectedMentoringDatas } = useApi({
+	// 	path: `/portfolio/mentor/mentoringRequests?status=rejected`,
+	// 	shouldFetch: true,
+	// });
+
+	// useEffect(() => {
+	// 	if (rejectedMentoringDatas && rejectedMentoringDatas.length > 0) {
+	// 		setRejectedMentoringData(rejectedMentoringDatas);
+	// 	}
+	// 	// console.log('(멘토)신청 거절 건: ', rejectedMentoringData);
+	// }, [rejectedMentoringDatas]);
+
+	// /// //////////////////////////////////////////
+	// // 유저 신청 완료 건
+	// const [requestedApplyData, setRequestedApplyData] = useState([]);
+	// const { result: requestedApplyDatas } = useApi({
+	// 	path: `/portfolio/user/myMentoringRequests?status=requested`,
+	// 	shouldFetch: true,
+	// });
+
+	// useEffect(() => {
+	// 	if (requestedApplyDatas && requestedApplyDatas.length > 0) {
+	// 		setRequestedApplyData(requestedApplyDatas);
+	// 	}
+	// 	// console.log('(유저)신청 완료 건: ', requestedApplyData);
+	// }, [requestedApplyDatas]);
+
+	// // 유저 진행 중인 건
+	// const [acceptedApplyData, setAcceptedApplyData] = useState([]);
+	// const { result: acceptedApplyDatas } = useApi({
+	// 	path: `/portfolio/user/myMentoringRequests?status=accepted`,
+	// 	shouldFetch: true,
+	// });
+
+	// useEffect(() => {
+	// 	if (acceptedApplyDatas && acceptedApplyDatas.length > 0) {
+	// 		setAcceptedApplyData(acceptedApplyDatas);
+	// 	}
+	// 	// console.log('(유저)진행 중인 건: ', acceptedApplyData);
+	// }, [acceptedApplyDatas]);
+
+	// // 유저 리뷰 완료 건
+	// const [completedApplyData, setCompletedApplyData] = useState([]);
+	// const { result: completedApplyDatas } = useApi({
+	// 	path: `/portfolio/user/myMentoringRequests?status=completed`,
+	// 	shouldFetch: true,
+	// });
+
+	// useEffect(() => {
+	// 	if (completedApplyDatas && completedApplyDatas.length > 0) {
+	// 		setCompletedApplyData(completedApplyDatas);
+	// 	}
+	// 	// console.log('(유저)리뷰 완료 건: ', completedApplyData);
+	// }, [completedApplyDatas]);
+
+	// // 유저 취소 건
+	// const [rejectedApplyData, setRejectedApplyData] = useState([]);
+	// const { result: rejectedApplyDatas } = useApi({
+	// 	path: `/portfolio/user/myMentoringRequests?status=rejected`,
+	// 	shouldFetch: true,
+	// });
+
+	// useEffect(() => {
+	// 	if (rejectedApplyDatas && rejectedApplyDatas.length > 0) {
+	// 		setRejectedApplyData(rejectedApplyDatas);
+	// 	}
+	// 	// console.log('(유저)취소 건: ', rejectedApplyData);
+	// }, [rejectedApplyDatas]);
+
+	// const [mentoringData, setMentoringData] = useRecoilState(mentoringItem);
+	// const [applyData, setApplyData] = useRecoilState(applyItem);
+	// useEffect(() => {
+	// 	// 모든 데이터가 로드되었는지 확인
+	// 	if (
+	// 		requestedMentoringData.length > 0 &&
+	// 		acceptedMentoringData.length > 0 &&
+	// 		completedMentoringData.length > 0 &&
+	// 		rejectedMentoringData.length > 0 &&
+	// 		requestedApplyData.length > 0 &&
+	// 		acceptedApplyData.length > 0 &&
+	// 		completedApplyData.length > 0 &&
+	// 		rejectedApplyData.length > 0
+	// 	) {
+	// 		// 모든 데이터가 로드되었다면 Recoil 상태 업데이트
+	// 		setMentoringData({
+	// 			requested: requestedMentoringData,
+	// 			accepted: acceptedMentoringData,
+	// 			completed: completedMentoringData,
+	// 			rejected: rejectedMentoringData,
+	// 		});
+	// 		setApplyData({
+	// 			requested: requestedApplyDatas,
+	// 			accepted: acceptedApplyDatas,
+	// 			completed: completedApplyDatas,
+	// 			rejected: rejectedApplyDatas,
+	// 		});
+	// 	}
+	// }, [
+	// 	requestedMentoringDatas,
+	// 	acceptedMentoringDatas,
+	// 	completedMentoringDatas,
+	// 	rejectedMentoringDatas,
+	// 	requestedApplyDatas,
+	// 	acceptedApplyDatas,
+	// 	completedApplyDatas,
+	// 	rejectedApplyDatas,
+	// ]);
 
 	return (
 		<M.Wrapper>

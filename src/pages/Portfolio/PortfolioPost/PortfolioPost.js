7a926c1b7e09a07fcd957ffae8713a2d0736d671
joayeon <codinz.n@gmail.com>
2023-09-01 05:21:21 +0900

    refactor: portfolioPost 코드 순서 변경

--- src/pages/Portfolio/PortfolioPost/PortfolioPost.js
+++ src/pages/Portfolio/PortfolioPost/PortfolioPost.js
@@ -1,83 +1,85 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
-import * as S from './PortfolioPost.styles';
 import useFooter from '../../../hooks/useFooter';
 import useApi from '../../../hooks/useApi';
+import { checkToken } from '../../../utils/cookie';
+import MESSAGE from '../../../constants/message';
+
+import * as S from './PortfolioPost.styles';
 
 import IntroContents from '../../../components/pages/Portfolio/IntroContents/IntroContents';
 import Review from '../../../components/@common/Review/Review';
 import Line from '../../../components/@common/Line/Line';
 import InfoEditModal from '../../../components/@common/ApplyModal/ApplyModal';
 import Button from '../../../components/@common/Button/Button';
-import MESSAGE from '../../../constants/message';
-import { checkToken } from '../../../utils/cookie';
 
 function PortfolioPost() {
-	useFooter(); // footer 제거
-
-	// user role 확인 후 멘토의 경우 수정/삭제 버튼 보여주고 / 일반 유저의 경우 신청하기 버튼 오픈
-	// 멘토 중에서도 작성자의 경우만 수정/삭제 버튼이 활성화 되게 로직 작성
-
-	// id 값
-	const params = useParams();
-	const path = params._id;
+	useFooter();
 
 	const navigate = useNavigate();
 
+	// params 가져오기
+	const params = useParams();
+	const path = params._id;
+
+	// 로그인 유무
+	const [isLoggedIn, setIsLoggedIn] = useState(checkToken());
+
 	const [post, setPost] = useState({});
 	const [userId, setUserId] = useState({});
-	const [isLoggedIn, setIsLoggedIn] = useState(checkToken());
+
+	// modal open state
 	const [infoModalOpenState, setInfoModalOpenState] = useState(false);
 
-	useEffect(() => {
-		const tokenStatus = checkToken();
-		setIsLoggedIn(tokenStatus);
-	}, []);
-
-	// 수정하기
+	// api 통신
 	const { result: userResult, error: userError } = useApi({
 		path: isLoggedIn ? '/user' : '',
 		shouldFetch: isLoggedIn,
 	});
 
-	useEffect(() => {
-		if (userResult) {
-			setUserId(userResult);
-			console.log(userError);
-		}
-	}, [userResult]);
-
-	// axios 통신 → 화면에 그려주는 작업
 	const { result, trigger, isLoading, error } = useApi({
 		path: `/portfolio/${path}`,
 		shouldFetch: true,
 	});
 
 	useEffect(() => {
+		const tokenStatus = checkToken();
+		setIsLoggedIn(tokenStatus);
+	}, []);
+
+	useEffect(() => {
 		if (result) {
 			setPost(result);
 			console.log(error);
 		}
-	}, [result]);
+
+		if (userResult) {
+			setUserId(userResult);
+			console.log(userError);
+		}
+	}, [result, userResult]);
 
 	// 버튼 클릭시 → 모달 open
 	const handleOpenModal = () => {
 		setInfoModalOpenState(true);
 	};
 
+	// 현재 로그인한 유저 아이디랑 게시글 오너 아이디랑 맞는지 확인
 	const userCheck = userId._id === post.ownerId;
+
+	// 로그인한 유저 아이디의 role 이 유저인지 확인
 	const roleCheck = userId.role === 'user';
 
+	// 게시글 수정
 	const handleEdit = () => {
 		if (confirm(MESSAGE.POST.EDIT)) {
 			navigate(`/portfolio/edit/${path}`);
 		}
 	};
 
-	// axios 통신 → 삭제하기 / 삭제 안 됨 (백엔드: userId 안 찍힘)
+	// 게시글 삭제
 	const handleDelete = () => {
-		// 삭제 메시지
 		if (confirm(MESSAGE.POST.DELETE)) {
 			trigger({ path: `/portfolio/${path}`, method: 'delete' });
 			alert(MESSAGE.DELETE.COMPLETE);
@@ -88,15 +90,19 @@ function PortfolioPost() {
 	const { nickName, title, updatedAt, profileImageUrl } = post;
 
 	// 날짜와 프로필 이미지
-	const serverDate = new Date(updatedAt);
-	const date = serverDate.toLocaleDateString('ko-KR');
-	const options = {
-		hour: 'numeric',
-		minute: 'numeric',
-		second: 'numeric',
-		hour12: false,
+	const dateAndTime = updatedAt => {
+		const serverDate = new Date(updatedAt);
+		const date = serverDate.toLocaleDateString('ko-KR');
+		const options = {
+			hour: 'numeric',
+			minute: 'numeric',
+			second: 'numeric',
+			hour12: false,
+		};
+		const time = serverDate.toLocaleTimeString('en-US', options);
+
+		return `${date} ${time}`;
 	};
-	const time = serverDate.toLocaleTimeString('en-US', options);
 
 	return (
 		<>
@@ -115,16 +121,12 @@ function PortfolioPost() {
 
 								<Line size={'height'} />
 
-								<span>
-									{date} &nbsp;{time}
-								</span>
+								<span>{dateAndTime(updatedAt)}</span>
 							</S.NameBox>
 
 							{infoModalOpenState && (
 								<InfoEditModal
-									setInfoModalOpenState={
-										setInfoModalOpenState
-									}
+									setInfoModalOpenState={setInfoModalOpenState}
 									path={path}
 									action={'완료'}
 								/>

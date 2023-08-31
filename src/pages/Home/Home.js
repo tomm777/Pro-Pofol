fdae80b019e77ad1920c03b18b3e5c76880cc3b0
[SW5] 이혜진 <meggylee792@gmail.com>
2023-08-31 21:17:59 +0900

    design: 방금 올라온 프로젝트/스터디 텍스트 수정

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -60,7 +60,7 @@ function Home() {
 				)}
 				<H.NewStudy>
 					<H.TitleBox>
-						<H.Title>🔥 방금 올라온 스터디/ 프로젝트</H.Title>
+						<H.Title>🔥 방금 올라온 프로젝트 / 스터디</H.Title>
 						<H.ViewAll href="/study">
 							전체보기
 							<img

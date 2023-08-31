a31227872ce1cdef0dfa775806c1c89b01225bf6
[SW5] 예은선 <ysvln0912@gmail.com>
2023-09-01 04:02:05 +0900

    feat: 무한 스크롤 진행 중

--- src/components/pages/StudyPage/StudyCategory/StudyCategory.js
+++ src/components/pages/StudyPage/StudyCategory/StudyCategory.js
@@ -18,10 +18,32 @@ function StudyCategory() {
 
 	// 무한스크롤
 	const [limit, setLimit] = useState(6);
-	const [skip, setSkip] = useState(0);
+	const [currentSkip, setCurrentSkip] = useState(6);
 
 	const observer = useRef();
 	const observerElement = useRef();
+
+	// 포지션 리스트
+	const {
+		trigger,
+		isLoading,
+		error,
+		result: positionResult,
+	} = useApi({
+		path: '/position',
+		shouldFetch: true,
+	});
+
+	// 카테고리에 맞는 스터디 프로젝트
+	const {
+		trigger: triggerProjectStudy,
+		isLoading: isLoadingProjectStudy,
+		error: errorProjectStudy,
+		result: resultProjectStudy,
+	} = useApi({
+		path: '/projectStudy',
+		shouldFetch: true,
+	});
 
 	// const handleObserver = entries => {
 	// 	const target = entries[0];
@@ -53,36 +75,49 @@ function StudyCategory() {
 	// 	}
 	// };
 
+	// ***********************************************
+
 	const handleObserver = entries => {
 		const target = entries[0];
 
 		if (target.isIntersecting && !isLoadingProjectStudy) {
-			console.log('통과');
-			setSkip(prevSkip => {
-				const newSkip = prevSkip + limit;
-				triggerProjectStudy({
-					path: '/projectStudy',
-					params: {
-						classification: selectedValues.classification,
-						position: selectedValues.position,
-						limit,
-						skip: newSkip,
-					},
-					shouldFetch: true,
-					applyResult: true,
-				});
+			console.log('플젝스터디1', projectStudy);
 
-				// setProjectStudy(prevProjectStudy => {
-				// 	return
-				// 	([...prevProjectStudy, ...resultProjectStudy]
-				// 	 console.log(prevProjectStudy);)
+			setCurrentSkip(prevSkip => {
+				return prevSkip + limit;
+				// triggerProjectStudy({
+				// 	path: '/projectStudy',
+				// 	params: {
+				// 		classification: selectedValues.classification,
+				// 		position: selectedValues.position,
+				// 		limit,
+				// 		skip: newSkip,
+				// 	},
+				// 	applyResult: true,
 				// });
-
-				console.log('PROJECT', projectStudy);
-
-				return newSkip;
 			});
+
+			console.log('LIMIT', limit, 'SKIP', currentSkip);
+
+			triggerProjectStudy({
+				path: '/projectStudy',
+				params: {
+					classification: selectedValues.classification,
+					position: selectedValues.position,
+					// limit,
+					skip: currentSkip,
+				},
+				applyResult: true,
+			});
+
+			setProjectStudy(prevProjectStudy => [
+				...prevProjectStudy,
+				...resultProjectStudy.projectStudies,
+			]);
+
+			console.log('스킵나온후플젝스터디5', projectStudy);
 		}
+		console.log('플젝스터디', projectStudy);
 	};
 
 	useEffect(() => {
@@ -105,27 +140,10 @@ function StudyCategory() {
 		};
 	}, [observer.current, observerElement]);
 
-	// 카테고리에 맞는 스터디 프로젝트
-	const {
-		trigger: triggerProjectStudy,
-		isLoading: isLoadingProjectStudy,
-		error: errorProjectStudy,
-		result: resultProjectStudy,
-	} = useApi({
-		path: '/projectStudy',
-		shouldFetch: true,
-	});
-
-	useEffect(() => {
-		if (resultProjectStudy && skip === 0) {
-			setProjectStudy(resultProjectStudy);
-		}
-	}, [resultProjectStudy]);
-
 	// 전체, 스터디, 프로젝트 클릭
 	const handleCategoryClick = classificationValue => {
 		setLimit(6);
-		setSkip(0);
+		// setCurrentSkip(0);
 
 		setSelectedValues(prev => ({
 			...prev,
@@ -146,7 +164,7 @@ function StudyCategory() {
 	// 포지션 클릭
 	const handlePositionClick = positionValue => {
 		setLimit(6);
-		setSkip(0);
+		// setSkip(0);
 
 		setSelectedValues(prev => ({
 			...prev,
@@ -164,22 +182,35 @@ function StudyCategory() {
 		});
 	};
 
-	// 포지션 리스트
-	const {
-		trigger,
-		isLoading,
-		error,
-		result: positionResult,
-	} = useApi({
-		path: '/position',
-		shouldFetch: true,
-	});
+	// useEffect(() => {
+	// 	console.log('STudyproject1', projectStudy);
+	// 	console.log('selectedValues', selectedValues);
+	// 	console.log('STudyproject2', projectStudy);
+	// }, [selectedValues]);
 
 	useEffect(() => {
-		if (positionResult && positionResult.length > 0) {
-			setPosition(positionResult);
+		if (positionResult.positions && positionResult.positions.length > 0) {
+			setPosition(positionResult.positions);
 		}
 	}, [positionResult]);
+
+	useEffect(() => {
+		console.log(
+			'resultProjectStudy.projectStudies222',
+			resultProjectStudy.projectStudies,
+		);
+
+		if (
+			resultProjectStudy.projectStudies &&
+			resultProjectStudy.projectStudies.length > 0 &&
+			currentSkip === 6
+		) {
+			console.log('projectStudy209', projectStudy);
+
+			console.log('처음set');
+			setProjectStudy(resultProjectStudy.projectStudies);
+		}
+	}, [resultProjectStudy]);
 
 	// ***********************************************************************
 	// ***********************************************************************

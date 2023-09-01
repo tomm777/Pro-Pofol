a432f07fca1ea3139eb585e43a61bd7d2995bd79
[SW5] 박민준 <nami47351@gmail.com>
2023-09-01 19:27:03 +0900

    fix: 유저 정보를 전역 store 에서 불러오기

--- src/components/pages/MyPage/RightContent/Mentor/MentoringPage/MentoringPage.js
+++ src/components/pages/MyPage/RightContent/Mentor/MentoringPage/MentoringPage.js
@@ -1,29 +1,16 @@
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilValue } from 'recoil';
 import * as MP from './MentoringPage.styles';
 import {
 	applyItem,
 	mentoringItem,
+	userItem,
 } from '../../../../../../recoil/atoms/myPage/myPage.atom';
 import MYPAGEOPTION from '../../../../../../constants/mypage';
-import { useEffect, useState } from 'react';
-import useApi from '../../../../../../hooks/useApi';
 
 // 멘토 코칭 페이지
 function MentoringPage() {
-	// 유저 정보 담을 state
-	const [user, setUser] = useState({});
-	// 유저 정보 통신(GET)
-	const { result: users } = useApi({
-		path: `/user`,
-		shouldFetch: true,
-	});
-
-	// 유저 정보가 변경될 때 리렌더링
-	useEffect(() => {
-		if (users) {
-			setUser(users);
-		}
-	}, [users]);
+	// 유저 데이터
+	const user = useRecoilValue(userItem);
 
 	// 멘토링 신청 받은 내역, 멘토링 신청 내역 데이터
 	const mentoringData = useRecoilValue(mentoringItem);
@@ -61,7 +48,7 @@ function MentoringPage() {
 			<MP.DetailOnboradWrapper>
 				<MP.MainTitleBox>
 					<MP.MainTitle>
-						{users.role === 'mentor'
+						{user.role === 'mentor'
 							? MYPAGEOPTION.MENTOR.TITLE
 							: MYPAGEOPTION.MENTEE.TITLE}
 					</MP.MainTitle>
@@ -70,12 +57,12 @@ function MentoringPage() {
 						{ApplicationArr.map((element, index) => (
 							<MP.DetailOnboradBox key={index}>
 								<MP.DetailOnboradSubTitle>
-									{users.role === 'mentor'
+									{user.role === 'mentor'
 										? element.subtitleMentor
 										: element.subtitleMentee}
 								</MP.DetailOnboradSubTitle>
 								<MP.DetailOnboradSubTitleCount>
-									{users.role === 'mentor'
+									{user.role === 'mentor'
 										? element.mentorLength
 										: element.userLength}
 								</MP.DetailOnboradSubTitleCount>

98d828892ce40752931457a764f3e70d438a3140
joayeon <codinz.n@gmail.com>
2023-09-01 13:06:59 +0900

    design: loading bar 디자인 작업

---
+++ src/components/@common/Loading/LoadingBar.js
@@ -0,0 +1,11 @@
+import * as S from './LoadingBar.styles';
+
+function LoadingBar() {
+	return (
+		<S.BarBox>
+			<img src="/assets/img/icons/loadingbar.gif" />
+		</S.BarBox>
+	);
+}
+
+export default LoadingBar;

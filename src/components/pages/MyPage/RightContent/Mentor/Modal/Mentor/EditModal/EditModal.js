5484e8d9cb6532a279777b13f50fec55755cc11f
[SW5] 박민준 <nami47351@gmail.com>
2023-09-01 19:27:44 +0900

    feat: 첨삭하기 기능 추가

--- src/components/pages/MyPage/RightContent/Mentor/Modal/Mentor/EditModal/EditModal.js
+++ src/components/pages/MyPage/RightContent/Mentor/Modal/Mentor/EditModal/EditModal.js
@@ -1,12 +1,12 @@
 import { useState, useRef, useEffect } from 'react';
 import * as EM from './EditModal.styles';
-import axios from 'axios';
 import Textarea from '../../../../../../../@common/Textarea/Textarea';
+import useApi from '../../../../../../../../hooks/useApi';
 
 function EditModal({ categoryKey, setEditModalOpenState, item }) {
 	const [textareaValue, setTextareaValue] = useState({
 		advice: '',
-		status: 'completed',
+		action: 'complete',
 	}); // 작성한 첨삭 내용 (멘토)
 	const [signupData, setSignupData] = useState([]); // 멘토링 신청 정보 (일반 유저)
 	const [mentoringData, setMentoringData] = useState([]); // 멘토링 작성 정보 (멘토)
@@ -21,41 +21,40 @@ function EditModal({ categoryKey, setEditModalOpenState, item }) {
 		});
 	};
 
+	// 유저 정보 통신(GET)
+	const { result, trigger } = useApi({
+		path: `/user`,
+		shouldFetch: true,
+	});
+
+	// 멘토
+	const portfolioId = item.portfolioId; // 멘토가 올린 신청 게시글의 id
+	const requestId = item._id; // 멘토가 신청 받은 id
+	const postData = textareaValue; // 수락할때 보내줄 데이터,
+
 	// 멘토가 입력한 정보 submit => post로 서버에 전달
 	const handleSubmit = e => {
 		e.preventDefault();
+
 		if (e.target[0].value === mentoringData.title) {
 			alert('변경 내용이 없습니다.');
 			closeModal();
 		} else {
-			// axios
-			// 	.post(
-			// 		'https://jsonplaceholder.typicode.com/posts',
-			// 		textareaValue,
-			// 	)
-			// 	.then(res => console.log(res))
-			// 	.then(alert('수정 완료되었습니다.'));
-			// .then(closeModal);
+			trigger({
+				method: 'post',
+				path: `/portfolio/mentor/respondToMentoringRequest/${portfolioId}/${requestId}`,
+				data: postData,
+				shouldFetch: true,
+			});
+			trigger({
+				method: 'get',
+				path: `/portfolio/mentor/mentoringRequests`,
+				shouldFetch: true,
+			});
+			alert('첨삭 되었습니다.');
+			window.location.replace('/mypage');
 		}
 	};
-
-	// 리뷰 완료처리된 첨삭 내용 불러오기
-	if (categoryKey === 'completed') {
-		useEffect(() => {
-			async function getMentoringData() {
-				try {
-					const response = await axios.get(
-						'https://jsonplaceholder.typicode.com/todos/1',
-					);
-					setMentoringData(response.data);
-					// console.log(response.data);
-				} catch (err) {
-					setError(err);
-				}
-			}
-			getMentoringData();
-		}, []);
-	}
 
 	// 모달 끄기
 	const closeModal = () => {

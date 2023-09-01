b3bf2badba8673fa0e6b6063506163f70f2a0f8d
[SW5] 이혜진 <meggylee792@gmail.com>
2023-09-01 16:08:22 +0900

    feat: 위로 가기 버튼 추가

--- src/components/@common/Layout/index.js
+++ src/components/@common/Layout/index.js
@@ -4,6 +4,7 @@ import Header from './Header/Header';
 import Footer from './Footer/Footer';
 import { useRecoilValue } from 'recoil';
 import { includeFooterState } from '../../../recoil/atoms/index.atom';
+import ScrollToTopButton from '../ScrollToTop/ScrollToTopButton';
 
 function Layout() {
 	// includeFooter가 true일 때만 Footer가 나타나도록 설정
@@ -12,6 +13,7 @@ function Layout() {
 		<>
 			<Header />
 			<Outlet />
+			<ScrollToTopButton />
 			{includeFooter && <Footer />}
 		</>
 	);

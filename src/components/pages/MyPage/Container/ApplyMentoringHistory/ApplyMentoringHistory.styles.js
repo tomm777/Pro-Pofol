30dc5d9ac2a0cb4e9f447302b3556829d2e32d45
[SW5] 박민준 <nami47351@gmail.com>
2023-09-07 17:46:25 +0900

    refactor: 멘토링 페이지 리팩토링

---
+++ src/components/pages/MyPage/Container/ApplyMentoringHistory/ApplyMentoringHistory.styles.js
@@ -0,0 +1,144 @@
+import styled from 'styled-components';
+
+export const DetailOnboradWrapper = styled.div`
+	display: inline-flex;
+	flex-direction: column;
+	align-items: flex-start;
+	gap: 2.5rem;
+	width: 100%;
+`;
+
+export const MainTitleBox = styled.div`
+	display: flex;
+	height: 9.875rem;
+	flex-direction: column;
+	align-items: flex-start;
+	gap: 2.5rem;
+	align-self: stretch;
+`;
+
+export const MainTitle = styled.div`
+	font-family: ${({ theme }) => theme.FONT_WEIGHT.bold};
+	font-size: ${({ theme }) => theme.FONT_SIZE.lg};
+`;
+
+export const DetailOnborad = styled.div`
+	display: flex;
+	height: 5.625rem;
+	padding: 1.875rem 3.75rem;
+	justify-content: center;
+	align-items: center;
+	gap: 6.25rem;
+	flex-shrink: 0;
+	align-self: stretch;
+
+	border-radius: 0.625rem;
+	border: 1px solid #000000;
+`;
+
+export const DetailOnboradBox = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: 0.25rem;
+`;
+
+export const DetailOnboradSubTitle = styled.span`
+	width: 5rem;
+	color: #616161;
+	text-align: center;
+	font-family: ${({ theme }) => theme.FONT_WEIGHT.light};
+	font-size: ${({ theme }) => theme.FONT_SIZE.xs};
+`;
+
+export const DetailOnboradSubTitleCount = styled.span`
+	color: #000;
+	font-family: Pretendard;
+	font-size: 1.875rem;
+	font-style: normal;
+	font-weight: 500;
+	line-height: normal;
+
+	font-family: ${({ theme }) => theme.FONT_WEIGHT.regular};
+	// font-size: ${({ theme }) => theme.FONT_SIZE.lg};
+	font-size: 1.875rem;
+`;
+
+// 카드 리스트
+export const SubContentBox = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	gap: 2rem;
+	align-self: stretch;
+`;
+
+export const SubContentBar = styled.div`
+	display: inline-flex;
+	justify-content: space-between;
+	align-items: center;
+	align-content: center;
+	flex-wrap: wrap;
+	width: 52.5rem;
+	border-bottom: 1px solid #ccc;
+`;
+
+export const SubTitleFlexBox = styled.div`
+	display: flex;
+	align-items: flex-start;
+`;
+
+export const SubTitle = styled.div`
+	display: flex;
+	width: 8rem;
+	padding: 0.625rem 1rem;
+	justify-content: center;
+	align-items: center;
+	gap: 0.625rem;
+`;
+
+export const Clicked = styled.div`
+	display: flex;
+	width: 8rem;
+	padding: 0.625rem 1rem;
+	justify-content: center;
+	align-items: center;
+	gap: 0.625rem;
+	font-family: ${({ theme }) => theme.FONT_WEIGHT.bold};
+	font-size: ${({ theme }) => theme.FONT_SIZE.sm};
+	cursor: pointer;
+`;
+
+export const NonClicked = styled.div`
+	display: flex;
+	width: 8rem;
+	padding: 0.625rem 1rem;
+	justify-content: center;
+	align-items: center;
+	gap: 0.625rem;
+
+	color: #ccc;
+	font-family: ${({ theme }) => theme.FONT_WEIGHT.bold};
+	font-size: ${({ theme }) => theme.FONT_SIZE.sm};
+	cursor: pointer;
+`;
+
+export const SubContentListBox = styled.div`
+	display: flex;
+	width: 52.5rem;
+	justify-content: flex-start;
+	align-items: flex-start;
+	align-content: flex-start;
+	row-gap: 1.25rem;
+	flex-wrap: wrap;
+	margin-bottom: 2rem;
+`;
+
+export const NonSubContentListBox = styled.div`
+	display: flex;
+	padding: 5rem 0.625rem;
+	justify-content: center;
+	align-items: center;
+	gap: 0.625rem;
+	align-self: stretch;
+`;
---
+++ src/components/pages/MyPage/Container/ApplyMentoringHistory/index.js
@@ -0,0 +1,170 @@
+import { useEffect, useMemo, useState } from 'react';
+import useApi from '../../../../../hooks/useApi';
+import * as AMH from './ApplyMentoringHistory.styles';
+import MYPAGEOPTION from '../../../../../constants/mypage';
+import { useRecoilValue } from 'recoil';
+import { userAtom } from '../../../../../recoil/atoms/index.atom';
+import MESSAGE from '../../../../../constants/message';
+import ApplicationCard from '../../Modules/Card/ApplicationCard';
+
+const countTitles = {
+	user: [
+		{ label: '신청 완료', value: 'requested' },
+		{ label: '멘토링 진행 중', value: 'accepted' },
+		{ label: '멘토링 진행 완료', value: 'completed' },
+		{ label: '신청 취소', value: 'rejected' },
+	],
+	mentor: [
+		{ label: '신청 받은 건', value: 'requested' },
+		{ label: '신청 수락 건', value: 'accepted' },
+		{ label: '리뷰 완료 건', value: 'completed' },
+		{ label: '신청 거절 건', value: 'rejected' },
+	],
+};
+
+const mapTypeToApiPath = {
+	user: '/portfolio/user/myMentoringRequests',
+	mentor: '/portfolio/mentor/mentoringRequests',
+};
+
+const ApplyMentoringHistory = ({ type }) => {
+	const {
+		result: statusResult,
+		isLoading,
+		trigger: statusTrigger,
+	} = useApi({
+		path: mapTypeToApiPath[type],
+		shouldFetch: true,
+	});
+
+	// console.log(statusResult);
+
+	const [tabIndex, setTabIndex] = useState(countTitles[type][0].value);
+
+	const statusObj = useMemo(() => {
+		if (Array.isArray(statusResult.mentoringRequests)) {
+			return statusResult.mentoringRequests?.reduce((acc, cur) => {
+				cur.status in acc
+					? (acc[cur.status] = acc[cur.status] + 1)
+					: (acc[cur.status] = 1);
+				return acc;
+			}, {});
+		}
+		return {};
+	}, [statusResult.mentoringRequests]);
+
+	const counts = useMemo(
+		() => [
+			'requested' in statusObj ? statusObj.requested : 0,
+			'accepted' in statusObj ? statusObj.accepted : 0,
+			'completed' in statusObj ? statusObj.completed : 0,
+			'rejected' in statusObj ? statusObj.rejected : 0,
+		],
+		[statusObj],
+	);
+
+	const getUnitStatusData = async status => {
+		try {
+			const result = await statusTrigger({
+				data: { status },
+				applyResult: false,
+				showBoundary: false,
+			});
+			console.log({ result });
+			setData(result.mentoringRequests);
+			setUserData(result.userInfos || result.UserInfos);
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	const handleClickTab = status => {
+		getUnitStatusData(status);
+	};
+
+	const initFetch = async () => {
+		getUnitStatusData(countTitles[type][0].value);
+	};
+
+	useEffect(() => {
+		initFetch();
+	}, []);
+
+	const user = useRecoilValue(userAtom);
+
+	const [data, setData] = useState([]);
+	const [userData, setUserData] = useState([]);
+	const [category, setCategory] = useState('requested');
+
+	useEffect(() => {
+		console.log(data);
+	}, [data]);
+
+	return (
+		<AMH.DetailOnboradWrapper>
+			<AMH.MainTitleBox>
+				<AMH.MainTitle>
+					{user?.role === 'mentor'
+						? MYPAGEOPTION.MENTOR.TITLE
+						: MYPAGEOPTION.MENTEE.TITLE}
+				</AMH.MainTitle>
+				<AMH.DetailOnborad>
+					{counts.map((count, index) => (
+						<AMH.DetailOnboradBox
+							key={`${countTitles[type][index].value}-count`}
+						>
+							<AMH.DetailOnboradSubTitle>
+								{countTitles[type][index].label}
+							</AMH.DetailOnboradSubTitle>
+							<AMH.DetailOnboradSubTitleCount>
+								{count}
+							</AMH.DetailOnboradSubTitleCount>
+						</AMH.DetailOnboradBox>
+					))}
+				</AMH.DetailOnborad>
+			</AMH.MainTitleBox>
+
+			<AMH.SubContentBox>
+				<AMH.SubContentBar>
+					<AMH.SubTitleFlexBox>
+						{countTitles[type].map(v => (
+							<AMH.Clicked
+								onClick={() => {
+									handleClickTab(v.value);
+									setCategory(v.value);
+								}}
+								key={`${v.value}-tab`}
+							>
+								{v.label}
+							</AMH.Clicked>
+						))}
+					</AMH.SubTitleFlexBox>
+
+					<p>총 {data.length}건</p>
+				</AMH.SubContentBar>
+
+				{data.length === 0 ? (
+					<AMH.NonSubContentListBox>
+						{MESSAGE.MYPAGE.NODATA.LIST}
+					</AMH.NonSubContentListBox>
+				) : (
+					<AMH.SubContentListBox>
+						{data.map((item, index) => {
+							return (
+								<ApplicationCard
+									key={index}
+									item={item}
+									userInfo={userData[index]}
+									category={category}
+									type={type}
+								></ApplicationCard>
+							);
+						})}
+					</AMH.SubContentListBox>
+				)}
+			</AMH.SubContentBox>
+		</AMH.DetailOnboradWrapper>
+	);
+};
+
+export default ApplyMentoringHistory;

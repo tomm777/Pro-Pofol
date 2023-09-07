d5ae915e5fe0a93fbc08fc5a0e29f0788d73989c
[SW5] 박민준 <nami47351@gmail.com>
2023-09-07 17:44:24 +0900

    feat: 마이페이지 outlet 추가

---
+++ src/components/pages/MyPage/Container/index.js
@@ -0,0 +1,5 @@
+export { default as ApplyMentoringHistory } from './ApplyMentoringHistory';
+export { default as EditedPostHistory } from './EditedPostHistory';
+export { default as ProjectStudy } from './ProjectStudy';
+export { default as AccountManage } from './AccountManage';
+export { default as AccountWithdrawal } from './AccountWithdrawal';

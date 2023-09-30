62c47b55334ece757df12ed79995d03ac4c5b6f2
joayeon <codinz.n@gmail.com>
2023-09-30 14:16:49 +0900

    fix: 파일 명 변경 후 경로 수정

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import * as H from './Home.styles';
-import RecommendCard from 'components/pages/Home/RecommendCard/RecommendCard';
+import RecommendCard from 'components/pages/Home/RecommendCard';
 import MentorCard from 'components/@common/Card';
 import RollingSlider from './SlideBanner/SlideBanner';
 import Slider from 'components/@common/Slider';

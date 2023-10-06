e72703ac3d48082f416c4b46c7153a6540231dc2
[SW5] 예은선 <ysvln0912@gmail.com>
2023-10-06 09:24:19 +0900

    refactor: 무한 스크롤 hook 분리 수정

--- src/components/pages/StudyPage/PostCardList/index.js
+++ src/components/pages/StudyPage/PostCardList/index.js
@@ -1,4 +1,76 @@
-import React, { useState, useEffect, useRef } from 'react';
+// import React, { useState, useEffect, useRef } from 'react';
+// import PostCard from '../PostCard';
+// import * as S from './index.styles';
+// import useInfiniteScroll from 'hooks/useInfiniteScroll';
+
+// import { useRecoilState } from 'recoil';
+// import { studyPageState } from 'recoil/atoms/studyPage.atom';
+
+// import LoadingBar from 'components/@common/Loading';
+// import EmptyMessage from 'components/@common/EmptyMessage';
+
+// function PostCardList({
+// 	isLoadingProjectStudy,
+// 	selectedValues,
+// 	trigger,
+// 	result,
+// 	setCurrentSkip,
+// 	currentSkip,
+// 	limit,
+// }) {
+// 	const [data, setData] = useRecoilState(studyPageState);
+
+// 	const loadData = () => {
+// 		console.log('통과할 때 콜백함수');
+// 		setCurrentSkip(prevSkip => {
+// 			return prevSkip + limit;
+// 		});
+
+// 		trigger({
+// 			path: '/projectStudies',
+// 			data: {
+// 				classification: selectedValues.classification,
+// 				position: selectedValues.position,
+// 				limit,
+// 				skip: currentSkip,
+// 			},
+// 			applyResult: true,
+// 		});
+
+// 		setData(prev => [...prev, ...result.projectStudies]);
+// 	};
+// 	const { observeElement } = useInfiniteScroll(loadData);
+
+// 	return (
+// 		<>
+// 			<S.PostCardContainer>
+// 				{isLoadingProjectStudy && !data.length && <LoadingBar />}
+// 				{!isLoadingProjectStudy && !data.length && <EmptyMessage />}
+// 				{Array.isArray(data) && data.length > 0 && (
+// 					<>
+// 						{data.map((data, idx) => (
+// 							<div key={data._id + idx}>
+// 								<PostCard data={data} />
+// 							</div>
+// 						))}
+
+// 						<div
+// 							style={{
+// 								height: '10px',
+// 								border: 'none',
+// 							}}
+// 							ref={observeElement}
+// 						/>
+// 					</>
+// 				)}
+// 			</S.PostCardContainer>
+// 		</>
+// 	);
+// }
+
+// export default PostCardList;
+
+import React, { useState, useEffect, useRef, useContext } from 'react';
 import PostCard from '../PostCard';
 import * as S from './index.styles';
 import useInfiniteScroll from 'hooks/useInfiniteScroll';
@@ -13,33 +85,40 @@ function PostCardList({
 	isLoadingProjectStudy,
 	selectedValues,
 	trigger,
-	result,
 	setCurrentSkip,
 	currentSkip,
 	limit,
 }) {
 	const [data, setData] = useRecoilState(studyPageState);
+	const [disableLoadData, setDisableLoadDate] = useState(false);
+	const observerRef = useRef();
 
-	const loadData = () => {
-		console.log('통과할 때 콜백함수');
-		setCurrentSkip(prevSkip => {
-			return prevSkip + limit;
-		});
-
-		trigger({
+	const loadData = async () => {
+		const nextSkip = currentSkip + limit;
+		setCurrentSkip(nextSkip);
+		if (disableLoadData) return;
+		const addedData = await trigger({
 			path: '/projectStudies',
 			data: {
 				classification: selectedValues.classification,
 				position: selectedValues.position,
 				limit,
-				skip: currentSkip,
+				skip: nextSkip,
 			},
-			applyResult: true,
+			applyResult: false,
 		});
-
-		setData(prev => [...prev, ...result.projectStudies]);
+		if (addedData && !addedData.projectStudies.length) {
+			setDisableLoadDate(true);
+		}
+		setData(prev => [...prev, ...addedData.projectStudies]);
 	};
-	const { observeElement } = useInfiniteScroll(loadData);
+
+	const { setTargetRef } = useInfiniteScroll(loadData, [currentSkip]);
+	useEffect(() => {
+		if (observerRef?.current) {
+			setTargetRef(observerRef);
+		}
+	}, [observerRef.current]);
 
 	return (
 		<>
@@ -53,16 +132,15 @@ function PostCardList({
 								<PostCard data={data} />
 							</div>
 						))}
-
-						<div
-							style={{
-								height: '10px',
-								border: 'none',
-							}}
-							ref={observeElement}
-						/>
 					</>
 				)}
+				<div
+					style={{
+						height: '10px',
+						border: 'none',
+					}}
+					ref={observerRef}
+				/>
 			</S.PostCardContainer>
 		</>
 	);
--- src/hooks/useInfiniteScroll.js
+++ src/hooks/useInfiniteScroll.js
@@ -1,43 +1,73 @@
+// import { useState, useEffect, useRef } from 'react';
+
+// // 범용적으로 사용 => 데이터를 불러오는 용도로만 사용하는 것이 아니기 때문에 func 와 같이 이름 수정하는 것
+// function useInfiniteScroll(func) {
+// 	const observer = useRef();
+
+// 	useEffect(() => {
+// 		const options = {
+// 			root: null,
+// 			rootMargin: '0px',
+// 			threshold: 1.0,
+// 		};
+
+// 		observer.current = new IntersectionObserver(entries => {
+// 			const target = entries[0];
+// 			// console.log(target, 'target', entries);
+// 			if (target.isIntersecting) {
+// 				func();
+// 			}
+// 		}, options);
+
+// 		return () => {
+// 			if (observer.current) {
+// 				observer.current.disconnect();
+// 			}
+// 		};
+// 	}, [func]);
+
+// 	const observeElement = useRef(null);
+// 	useEffect(() => {
+// 		if (observeElement.current && observer.current) {
+// 			observer.current.observe(observeElement.current);
+// 		}
+// 		return () => {
+// 			if (observeElement.current && observer.current) {
+// 				observer.current.unobserve(observeElement.current);
+// 			}
+// 		};
+// 	}, [observeElement]);
+
+// 	return { observeElement };
+// }
+
+// export default useInfiniteScroll;
+
 import { useState, useEffect, useRef } from 'react';
 
-function useInfiniteScroll(loadData) {
-	const observer = useRef();
+function useInfiniteScroll(loadData, deps) {
+	const [targetRef, setTargetRef] = useState(useRef(null));
+
+	const intersectionObserver = new IntersectionObserver(entries => {
+		const target = entries[0];
+		if (target.isIntersecting) {
+			intersectionObserver.unobserve(targetRef.current);
+			loadData();
+		}
+	});
 
 	useEffect(() => {
-		const options = {
-			root: null,
-			rootMargin: '0px',
-			threshold: 1.0,
-		};
-
-		observer.current = new IntersectionObserver(entries => {
-			const target = entries[0];
-			// console.log(target, 'target', entries);
-			if (target.isIntersecting) {
-				loadData();
-			}
-		}, options);
-
-		return () => {
-			if (observer.current) {
-				observer.current.disconnect();
-			}
-		};
-	}, [loadData]);
-
-	const observeElement = useRef(null);
-	useEffect(() => {
-		if (observeElement.current && observer.current) {
-			observer.current.observe(observeElement.current);
+		if (targetRef?.current) {
+			intersectionObserver.observe(targetRef.current);
 		}
 		return () => {
-			if (observeElement.current && observer.current) {
-				observer.current.unobserve(observeElement.current);
+			if (targetRef?.current && targetRef.current) {
+				intersectionObserver.disconnect();
 			}
 		};
-	}, [observeElement]);
+	}, [targetRef.current, ...deps]);
 
-	return { observeElement };
+	return { setTargetRef };
 }
 
 export default useInfiniteScroll;
